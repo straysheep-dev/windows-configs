dc68bd31b52f0393f380827ccf0263dc68312413
straysheep-dev <bah@straysheep.dev>
2022-08-22 23:31:23 -0700

    Create Set-WinPrefsUser

---
+++ Set-WinPrefsUser.ps1
@@ -0,0 +1,150 @@
+##########
+# Win 10 / Server 2016 / Server 2019 Initial Setup Script - Tweak library
+# Author: Disassembler <disassembler@dasm.cz>
+# Source: https://github.com/Disassembler0/Win10-Initial-Setup-Script
+# Forked from version: v3.10, 2020-07-15
+##########
+
+##########
+#region Privacy Tweaks
+##########
+
+
+# Disable Cortana
+
+Write-Output "Disabling Cortana..."
+
+If (!(Test-Path "HKCU:\Software\Microsoft\Personalization\Settings")) {
+	New-Item -Path "HKCU:\Software\Microsoft\Personalization\Settings" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+
+If (!(Test-Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore")) {
+	New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+
+# Taskbar > Right click Cortana Icon (Show|Hide)
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCortanaButton" -Type DWord -Value 0
+
+# Disable Application suggestions and automatic installation
+
+Write-Output "Disabling Application suggestions..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "ContentDeliveryAllowed" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "OemPreInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "PreInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "PreInstalledAppsEverEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SilentInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SystemPaneSuggestionsEnabled" -Type DWord -Value 0
+
+
+# Disable Tailored Experiences
+
+Write-Output "Disabling Tailored Experiences..."
+If (!(Test-Path "HKCU:\Software\Policies\Microsoft\Windows\CloudContent")) {
+	New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\CloudContent" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\CloudContent" -Name "DisableTailoredExperiencesWithDiagnosticData" -Type DWord -Value 1
+
+
+# Disable setting 'Let websites provide locally relevant content by accessing my language list'
+
+Write-Output "Disabling Website Access to Language List..."
+Set-ItemProperty -Path "HKCU:\Control Panel\International\User Profile" -Name "HttpAcceptLanguageOptOut" -Type DWord -Value 1
+
+##########
+#endregion Privacy Tweaks
+##########
+
+
+##########
+#region Service Tweaks
+##########
+
+
+# Disable Autoplay
+
+Write-Output "Disabling Autoplay..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
+
+
+##########
+#endregion Service Tweaks
+##########
+
+
+
+##########
+#region UI Tweaks
+##########
+
+
+# Disable Aero Shake (minimizing other windows when one is dragged by mouse and shaken)
+
+#Write-Output "Disabling Aero Shake..."
+#Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "DisallowShaking" -Type DWord -Value 1
+
+
+# Disable accessibility keys prompts (Sticky keys, Toggle keys, Filter keys)
+
+Write-Output "Disabling accessibility keys prompts..."
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\ToggleKeys" -Name "Flags" -Type String -Value "58"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\Keyboard Response" -Name "Flags" -Type String -Value "122"
+
+
+# Set Dark Mode for Applications
+
+Write-Output "Setting Dark Mode for Applications..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Type DWord -Value 0
+
+
+# Set Dark Mode for System - Applicable since 1903
+
+Write-Output "Setting Dark Mode for System..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Type DWord -Value 0
+
+
+##########
+#endregion UI Tweaks
+##########
+
+
+
+##########
+#region Explorer UI Tweaks
+##########
+
+
+# Show known file extensions
+
+Write-Output "Showing known file extensions..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
+
+
+# Show hidden files
+
+Write-Output "Showing hidden files..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 1
+
+
+##########
+#endregion Explorer UI Tweaks
+##########
+
+
+
+
+
+##########
+#region Application Tweaks
+##########
+
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\GameBar" -Name "AutoGameModeEnabled" -Type DWord -Value 0
+#Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_Enabled" -Type DWord -Value 0
+
+
+##########
+#endregion Application Tweaks
+##########
