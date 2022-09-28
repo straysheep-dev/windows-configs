c6b7efcb3f795894146b9866c0fa628bb9f41881
straysheep-dev <bah@straysheep.dev>
2022-09-27 20:14:20 -0700

    Update Manage-Sysinternals

--- Manage-Sysinternals.ps1
+++ Manage-Sysinternals.ps1
@@ -14,6 +14,9 @@ If(!(Test-Path -LiteralPath "C:\Tools")) {
 	icacls.exe C:\Tools /grant *S-1-1-0:"(CI)(OI)RX" > $null
 }
 
+Write-Host -BackgroundColor Blue "Copying .\Manage-Sysinternals.ps1 -> C:\Tools..."
+Copy-Item -Path ".\Manage-Sysinternals.ps1" -Destination "C:\Tools"
+
 # Prompt to download entire Sysinternals Suite or skip
 while ( $DownloadSysinternalsSuite -ne "y" -or "n" ) {
 	if ( $DownloadSysinternalsSuite -eq "y" ) {
@@ -31,13 +34,85 @@ while ( $DownloadSysinternalsSuite -ne "y" -or "n" ) {
 
 # sigcheck
 If(!(Test-Path -LiteralPath "C:\Tools\sigcheck64.exe")) {
-	Write-Host -BackgroundColor Blue "Downloading SigCheck..."
+	Write-Host -BackgroundColor Blue "Downloading sigcheck64..."
 	iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/sigcheck64.exe', 'C:\Tools\sigcheck64.exe')"
 }
 
-# procexp
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.2#section-1
+# Until PowerShell 7, containing a statement within parentheses (...), subexpression syntax $(...) or array expression @(...) always
+# reset $? to True, so that (Write-Error) shows $? as True. This has been changed in PowerShell 7, so that $? always reflects the actual
+# success of the last command run in these expressions.
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-7.2#lastexitcode
+# Contains the exit code of the last native program or PowerShell script that was run.
+
+Write-Host "
+	* accesschk64
+	* Autoruns64
+	* tcpview64
+	* strings64
+	* vmmap64
+	* whois64
+"
+
+# Prompt to download essentials or skip
+while ( $DownloadEssentials -ne "y" -or "n" ) {
+	if ( $DownloadEssentials -eq "y" ) {
+		Write-Host -BackgroundColor Blue "Downloading selected tools..."
+
+		If(!(Test-Path -LiteralPath "C:\Tools\accesschk64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading accesschk64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/accesschk64.exe', 'C:\Tools\accesschk64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\accesschk64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+
+		If(!(Test-Path -LiteralPath "C:\Tools\Autoruns64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading Autoruns64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/Autoruns64.exe', 'C:\Tools\Autoruns64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\Autoruns64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+
+		If(!(Test-Path -LiteralPath "C:\Tools\tcpview64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading tcpview64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/tcpview64.exe', 'C:\Tools\tcpview64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\tcpview64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+
+		If(!(Test-Path -LiteralPath "C:\Tools\strings64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading strings64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/strings64.exe', 'C:\Tools\strings64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\strings64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+
+		If(!(Test-Path -LiteralPath "C:\Tools\vmmap64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading vmmap64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/vmmap64.exe', 'C:\Tools\vmmap64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\vmmap64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+
+		If(!(Test-Path -LiteralPath "C:\Tools\whois64.exe")) {
+			Write-Host -BackgroundColor Blue "Downloading whois64..."
+			iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/whois64.exe', 'C:\Tools\whois64.exe')"
+			C:\Tools\sigcheck64.exe -accepteula C:\Tools\whois64.exe
+			if ($LASTEXITCODE -eq 1) { Write-Host -ForegroundColor Red -BackgroundColor Yellow "[i]SIGNATURE ERROR" }
+		}
+		break
+	}
+	elseif ( $DownloadEssentials -eq "n" ) {
+		break
+	}
+	else {
+		$DownloadEssentials = Read-Host "Download just these tools? [y/n]"
+	}
+}
+
 If(!(Test-Path -LiteralPath "C:\Tools\procexp64.exe")) {
-	Write-Host -BackgroundColor Blue "Downloading Process Explorer..."
+	Write-Host -BackgroundColor Blue "Downloading Process Explorer x64..."
 	iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/procexp64.exe', 'C:\Tools\procexp64.exe')"
 
 	# Check signature
@@ -60,11 +135,14 @@ If(!(Test-Path -LiteralPath "C:\Tools\procexp64.exe")) {
 		}
 	}
 
-	# Replace taskmgr
+	# Replace Task Manager with Process Explorer
 	Write-Host ""
 
 	while ( $ReplaceTaskMgr -ne "y" -or "n" ) {
 		if ( $ReplaceTaskMgr -eq "y" ) {
+			If(!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe")) {
+				New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe"
+			}
 			Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe\" -Name Debugger -Type String -Value "C:\TOOLS\PROCEXP64.EXE"
 			break
 		}
@@ -78,7 +156,7 @@ If(!(Test-Path -LiteralPath "C:\Tools\procexp64.exe")) {
 }
 
 Write-Host -BackgroundColor Blue "Updating Sysmon..."
-Write-Host -BackgroundColor Blue "Downloading latest Sysmon binary..."
+Write-Host -BackgroundColor Blue "Downloading latest Sysmon x64 binary..."
 iex "(New-Object Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe', 'C:\Tools\Sysmon64.exe')"
 
 while ( $ExistingConfig -ne "y" -or "n" ) {
@@ -92,10 +170,10 @@ while ( $ExistingConfig -ne "y" -or "n" ) {
 		If(!(Get-FileHash "C:\Tools\sysmon-config.xml" | Select-String '055febc600e6d7448cdf3812307275912927a62b1f94d0d933b64b294bc87162')) {
 			Write-Host -BackgroundColor Red "sysmon-config.xml hash verification failed. Quitting."
 			Start-Sleep 3
-			exit
+			exit 1
 		}
 		else {
-			Write-Host -BackgroundColor Green "sha256sum OK"
+			Write-Host -ForegroundColor DarkBlue -BackgroundColor Green "sha256sum OK"
 		}
 		break
 	}
