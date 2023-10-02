81e36291704d011f2217c6ece80051bee7779386
straysheep-dev <84547874+straysheep-dev@users.noreply.github.com>
2023-10-01 23:51:32 -0700

    Update generic-sandbox-logon.cmd

--- generic-sandbox-logon.cmd
+++ generic-sandbox-logon.cmd
@@ -3,4 +3,5 @@ explorer.exe C:\Users\WDAGUtilityAccount\Documents\Tools
 
 REM Enable script execution
 cmd.exe /C powershell.exe -c Set-ExecutionPolicy Bypass -Force
+REM Execute scripts
 cmd.exe /C powershell.exe C:\Users\WDAGUtilityAccount\Documents\Tools\generic-sandbox-setup.ps1
