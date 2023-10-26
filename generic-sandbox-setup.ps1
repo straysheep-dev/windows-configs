6672401aff45a174421518b4921baf77858e7099
straysheep-dev <84547874+straysheep-dev@users.noreply.github.com>
2023-10-25 17:15:13 -0700

    Update and rename generic-sandbox-setup.ps1 to sandbox-generic-setup.ps1

--- generic-sandbox-setup.ps1
+++
@@ -1,3 +0,0 @@
-# Add any PowerShell commands to this file
-Import-Module C:\Users\WDAGUtilityAccount\Documents\Tools\Set-EdgePolicy.ps1
-Set-EdgePolicy Apply
---
+++ sandbox-generic-setup.ps1
@@ -0,0 +1,3 @@
+# Add any PowerShell commands to this file
+Import-Module C:\Users\WDAGUtilityAccount\Documents\Sandbox-Tools\Set-EdgePolicy.ps1
+Set-EdgePolicy Apply
