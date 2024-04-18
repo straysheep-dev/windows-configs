f9294e82a1f4bbcbe123a7bc0f2d6760277b1d85
straysheep-dev <bah@straysheep.dev>
2024-04-18 01:42:49 -0700

    New Windows Sandbox files

---
+++ sandbox-developer-logon.cmd
@@ -0,0 +1,8 @@
+REM Open tools folder
+explorer.exe C:\Users\WDAGUtilityAccount\Documents\Sandbox-Tools
+
+REM Enable script execution
+cmd.exe /C powershell.exe -c Set-ExecutionPolicy Bypass -Force
+REM Execute scripts
+cmd.exe /C powershell.exe C:\Users\WDAGUtilityAccount\Documents\Sandbox-Tools\sandbox-generic-setup.ps1
+cmd.exe /C powershell.exe C:\Users\WDAGUtilityAccount\Documents\Sandbox-Tools\sandbox-developer-setup.ps1
---
+++ sandbox-wireguard-setup.ps1
@@ -0,0 +1,2 @@
+# Install the latest Wireguard client
+cd $env:TEMP; iwr https://download.wireguard.com/windows-client/wireguard-installer.exe -OutFile wireguard-installer.exe; .\wireguard-installer.exe
